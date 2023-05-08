1cf15adbeeb308f58ad4d6444f88e6d4f55d1d25
kana.hashimoto <kana.hashimoto@j-q.co.jp>
2023-05-08 11:45:48 +0900

    FrontController作成

--- src/main/java/constants/ForwardConst.java
+++ src/main/java/constants/ForwardConst.java
@@ -30,7 +30,7 @@ public enum ForwardConst {
     FW_LOGIN("login/login"),
     FW_EMP_INDEX("employees/index"),
     FW_EMP_SHOW("employees/show"),
-    FW_ENP_NEW("employees/new"),
+    FW_EMP_NEW("employees/new"),
     FW_EMP_EDIT("employees/edit"),
     FW_REP_INDEX("reports/index"),
     FW_REP_SHOW("reports/show"),
